7dd36cbec7ce37e7172af0656b1dad53579c3b5a
Varian <mizeyuyu@gmail.com>
2020-06-07 23:53:30 +0800

    更新命令列表

--- cli.js
+++ cli.js
@@ -8,17 +8,19 @@ program
   .version(pkg.version)
 program
   .option('-A, --add', '添加任务')
+program
+  .option('-C, --clear', '清空任务')
 
 program
-  .command('add')
-  .description('add task')
+  .command('add <taskName>')
+  .description('添加任务')
   .action((obj) => {
     api.add(obj.args.join(' ')).then(() => console.log('添加成功！'), () => console.log('添加失败！'))
   })
 
 program
   .command('clear')
-  .description('clear tasks')
+  .description('清空任务')
   .action(() => {
     api.clear().then(() => console.log('清除成功！'), () => console.log('清除失败！'))
   })
